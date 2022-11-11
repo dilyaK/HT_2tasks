a15d4f072ab00547e36b070fa1bd909dc82d4d58
dilya <1dilnora777@gmail.com>
2022-11-11 12:26:56 +0500

    task002 added

---
+++ Task001_10/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на входе 
+// трехзначное число и на выходе показывает 2-цифру этого числа:
+// 456 -> 5;
+// 782 -> 8;
+
+/*int  result = 0;
+Console.WriteLine("Enter a three-digit number: ");
+int  numA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" The second digit is:  ");
+result = numA / 10 % 10;
+Console.Write(result);*/
+
+// Напишите программу, которая выводит 3-цифру 
+// заданного числа или сообщает что 3-цифры нет.
+// 645 -> 5;
+// 78 -> 3-цифры нет;
+// 37679 -> 6;
+
+
+int result = 0;
+Console.WriteLine("Enter a number: ");
+
+int numA = Convert.ToInt32(Console.ReadLine());
+if (numA < 100)
+{
+    Console.WriteLine("NO THIRD DIGIT");
+}
+
+else
+{
+
+        result = numA / 10 % 10;
+    
+    Console.WriteLine(" The third digit is: [result]");
+    Console.Write(result);
+}
---
+++ Task002_13/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит 3-цифру 
+// заданного числа или сообщает что 3-цифры нет.
+// 645 -> 5;
+// 78 -> 3-цифры нет;
+// 37679 -> 6
+
+int numA = 37679;
+result = 0;
+
+Console.WriteLine("Enter five digit number: ");
+//int numA = Convert.ToInt32(Console.ReadLine());
+
+if (numA < 100)
+{
+    Console.WriteLine("NO THIRD DIGIT");
+}
+
+else
+{
+    Console.WriteLine($"The third digit is ");
+    result = numA / 100 % 10;
+    Console.WriteLine(result);
+
+}
+
+
